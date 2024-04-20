c5bb52647fa0b05715e75ff13acfc82bb169a131
Sean Gilbank <sean7725@hotmail.com>
2024-04-19 22:59:07 -0700

    updated results javascript to fetch the data

--- RESULTS_SUMMARY_COMPONENT/index.js
+++ RESULTS_SUMMARY_COMPONENT/index.js
@@ -1,16 +1,19 @@
-import data from './data.json' assert { type: 'json' }
-
-const summaryCompEl = document.getElementById('summary-comp')
-
-// map through the data and create a new array of objects
-const summary = data.map((item) => { 
-    return `
+// Fetch the JSON data from the local JSON file
+fetch('./data.json')
+  .then(response => response.json())
+  .then(data => {
+    // Process the data
+    const summaryCompEl = document.getElementById('summary-comp');
+    const summary = data.map(item => {
+      return `
         <div class="${item.class} card flex">
             <img id="icon" src=${item.icon} alt=${item.alt}>
-            <span id="category" class=" ${item.color}">${item.category}</span>
+            <span id="category" class="${item.color}">${item.category}</span>
             <span id="score" class="score">${item.score}</span> / 100
         </div>
-    `
-})
-// render the summary array to the DOM
-summaryCompEl.innerHTML = summary.join('')
+      `;
+    });
+    // Render the summary array to the DOM
+    summaryCompEl.innerHTML = summary.join('');
+  })
+  .catch(error => console.error('Error loading the JSON file:', error));
